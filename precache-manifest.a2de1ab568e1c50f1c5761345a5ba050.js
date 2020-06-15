8d83f5654265869e13aff68ab1b501b7e939904c
Ghost <gabriel17@live.com.pt>
2020-06-16 00:14:44 +0100

    Updates

---
+++ precache-manifest.a2de1ab568e1c50f1c5761345a5ba050.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8df6c716440cd2ad4062c017e46ed2e",
+    "url": "/nearearthh/index.html"
+  },
+  {
+    "revision": "ce66e071273d6749717f",
+    "url": "/nearearthh/static/css/main.8effb0e4.chunk.css"
+  },
+  {
+    "revision": "7d0733f3982c28dc01bd",
+    "url": "/nearearthh/static/js/2.9282e7cc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nearearthh/static/js/2.9282e7cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce66e071273d6749717f",
+    "url": "/nearearthh/static/js/main.b43541b1.chunk.js"
+  },
+  {
+    "revision": "be811024f8cf21c16394",
+    "url": "/nearearthh/static/js/runtime-main.6052ba87.js"
+  }
+]);
